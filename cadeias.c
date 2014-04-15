f5a4b12625ce250efd2c1b391c5945e973f4a2e0
_bpp <pereirabruno05@gmail.com>
2014-04-15 15:39:29 +0100

    código de cadeias.c terminado

--- cadeias.c
+++ cadeias.c
@@ -60,265 +60,245 @@ int cad_ocorrencias ( char c , char s [])
     return cont;
 }
 
-/*TODO int cad_procurar_str ( char s1 [] , char s2 []);*/
 
 int cad_procurar_str ( char s1 [] , char s2 [])
 {
-
-    int i, x;
-
-
-    for (i =0; s2[i] ; i++);
-
-    for(x = 0; s2 [i] == s1 [i+x] ; x++);
-
-    /*if(s1 [i] != s2 [j+x])*/
-    /*enc = 0;*/
-
-
-
-    /*if(enc)*/
-    return i;
-    /*else*/
-    /*    return -1;*/
-}
-
-
-
-/**
- * Esta função verifica se o segundo argumento contém o primeiro.
- * Por exemplo, verifica se "banana" contém "ana"
- * @return 1 se maior contém pequena, 0 caso contrário
- * @param maior A string maior
- * @param pequena A string mais pequena
- * */
-int cad_e_prefixo ( char s1 [] , char s2 [])
-{
-
-    int i , e_pref;
-
-    for(i=0, e_pref = 1; s1[i]&&e_pref; i++) {
-        if(s1[i]!= s2[i]) e_pref = 0;
-
-
+    int i, j;
+    for (i =0; s2[i]; i++){
+            for(j = 0; s2[j]&&(s2 [j+i] == s1 [j]) ; j++);
+                if(s1 [j]=='\0')
+                    return i;
     }
-    return e_pref;
-
+                return -1;
+            
 }
 
-/**
- * Esta função verifica se o segundo argumento contém o primeiro.
- * Por exemplo, verifica se "banana" contém "ana"
- * @return 1 se maior contém pequena, 0 caso contrário
- * @param maior A string maior
- * @param pequena A string mais pequena
- * */
-int cad_e_sufixo ( char s1 [] , char s2 [])
-{
-
-    int i, j, e_suf = 1;
 
 
-    i = cad_tamanho (s1) - 1;
-    j = cad_tamanho (s2) - 1;
+    /**
+     * Esta função verifica se o segundo argumento contém o primeiro.
+     * Por exemplo, verifica se "banana" contém "ana"
+     * @return 1 se maior contém pequena, 0 caso contrário
+     * @param maior A string maior
+     * @param pequena A string mais pequena
+     * */
+    int cad_e_prefixo ( char s1 [] , char s2 []) {
+
+        int i , e_pref;
+
+        for(i=0, e_pref = 1; s1[i]&&e_pref; i++) {
+            if(s1[i]!= s2[i]) e_pref = 0;
 
 
-
-    while(i>=0&&j>=0&&e_suf) {
-
-        if (s1[i]!=s2[j])
-            e_suf = 0;
-
-        i--;
-        j--;
+        }
+        return e_pref;
 
     }
 
-    return e_suf&&i==-1;
-}
+    /**
+     * Esta função verifica se o segundo argumento contém o primeiro.
+     * Por exemplo, verifica se "banana" contém "ana"
+     * @return 1 se maior contém pequena, 0 caso contrário
+     * @param maior A string maior
+     * @param pequena A string mais pequena
+     * */
+    int cad_e_sufixo ( char s1 [] , char s2 []) {
+
+        int i, j, e_suf = 1;
+
+
+        i = cad_tamanho (s1) - 1;
+        j = cad_tamanho (s2) - 1;
 
 
 
-int cad_iguais ( char s1 [] , char s2 [])
-{
+        while(i>=0&&j>=0&&e_suf) {
 
-    int i, igual = 1;
-    int size1 = cad_tamanho (s1);
-    int size2 = cad_tamanho (s2);
-    if(size1==size2) {
-        for(i = 0; s1[i]&&s2[i]&&igual; i++)
-            if(s1[i]!=s2[i])
-                igual = 0;
-    } else
-        igual = 0;
+            if (s1[i]!=s2[j])
+                e_suf = 0;
 
+            i--;
+            j--;
 
+        }
+
+        return e_suf&&i==-1;
+    }
 
 
-    return igual;
-}
-/**
- * Esta função verifica se o segundo argumento contém o primeiro.
- * Por exemplo, verifica se "banana" contém "ana"
- * @return 1 se maior contém pequena, 0 caso contrário
- * @param maior A string maior
- * @param pequena A string mais pequena
- * */
-int cad_primeiro ( char s1 [] , char s2 [])
-{
 
-    int size1, size2, e_menor = 0;
+    int cad_iguais ( char s1 [] , char s2 []) {
 
-    size1 = cad_tamanho (s1);
-    size2 = cad_tamanho (s2);
-
-    int i = 0;
+        int i, igual = 1;
+        int size1 = cad_tamanho (s1);
+        int size2 = cad_tamanho (s2);
+        if(size1==size2) {
+            for(i = 0; s1[i]&&s2[i]&&igual; i++)
+                if(s1[i]!=s2[i])
+                    igual = 0;
+        } else
+            igual = 0;
 
 
-    while(s1[i]&&s2[i]) {
 
-        if(s1 [i] == s2 [i]) {
-            i++;
-            continue;
-        } else if(s1 [i] < s2 [i])
+
+        return igual;
+    }
+    /**
+     * Esta função verifica se o segundo argumento contém o primeiro.
+     * Por exemplo, verifica se "banana" contém "ana"
+     * @return 1 se maior contém pequena, 0 caso contrário
+     * @param maior A string maior
+     * @param pequena A string mais pequena
+     * */
+    int cad_primeiro ( char s1 [] , char s2 []) {
+
+        int size1, size2, i = 0;
+
+        size1 = cad_tamanho (s1);
+        size2 = cad_tamanho (s2);
+
+
+
+
+        while(s1[i]&&s2[i]) {
+
+            if(s1 [i] == s2 [i]) {
+                i++;
+                continue;
+            } else if(s1 [i] < s2 [i])
+                return 1;
+            else
+                return 0;
+        }
+
+        if (size1 < size2)
             return 1;
         else
             return 0;
     }
 
-    if (size1 < size2)
-        return 1;
-    else
-        return 0;
-}
 
 
 
+    int cad_comparar ( char s1 [] , char s2 []) {
 
-int cad_comparar ( char s1 [] , char s2 [])
-{
+        int i = 0;
+        int size1 = cad_tamanho(s1);
+        int size2 = cad_tamanho(s2);
+        while(s1[i]&&s2[i]) {
 
-    int i = 0;
-    int size1 = cad_tamanho(s1);
-    int size2 = cad_tamanho(s2);
-    while(s1[i]&&s2[i]) {
-
-        if(s1 [i] == s2 [i]) {
-            i++;
-            continue;
-        }
-        if(s1 [i] < s2 [i])
-            return -1;
-        if(s1 [i] > s2 [i])
-            return 1;
-    }
-    if(size1 > size2)
-        return 1;
-    else if (size1 < size2)
-        return -1;
-    else
-        return 0;
-}
-
-/**
- * Esta função verifica se o segundo argumento contém o primeiro.
- * Por exemplo, verifica se "banana" contém "ana"
- * @return 1 se maior contém pequena, 0 caso contrário
- * @param maior A string maior
- * @param pequena A string mais pequena
- * */
-int cad_um_dos ( char s1 [] , char s2 [])
-{
-    int i , j, index, enc = 0;
-
-
-    
-   
-
-    for(i = 0; !enc&&s1[i]; i++) {
-        for(j =0; !enc&&s2[j]; j++)
-            if(s1[i]==s2[j]) {
-                enc = 1;
-                index = i;
-
+            if(s1 [i] == s2 [i]) {
+                i++;
+                continue;
             }
+            if(s1 [i] < s2 [i])
+                return -1;
+            if(s1 [i] > s2 [i])
+                return 1;
+        }
+        if(size1 > size2)
+            return 1;
+        else if (size1 < size2)
+            return -1;
+        else
+            return 0;
     }
 
-    if(enc)
-        return index;
-    else
-        return -1;
-}
-/**
- * Esta função verifica se o segundo argumento contém o primeiro.
- * Por exemplo, verifica se "banana" contém "ana"
- * @return 1 se maior contém pequena, 0 caso contrário
- * @param maior A string maior
- * @param pequena A string mais pequena
- * */
-int cad_nenhum_dos ( char s1 [] , char s2 [])
-{
+    /**
+     * Esta função verifica se o segundo argumento contém o primeiro.
+     * Por exemplo, verifica se "banana" contém "ana"
+     * @return 1 se maior contém pequena, 0 caso contrário
+     * @param maior A string maior
+     * @param pequena A string mais pequena
+     * */
+    int cad_um_dos ( char s1 [] , char s2 []) {
+        int i , j, index, enc = 0;
 
 
-    int i , enc = 0;
-
-    for(i=0; s1[i]==s2[i]&&s1[i]&&s2[i]; i++);
-
-    if(s1[i]!=s2[i])
-        enc = 1;
 
 
-    if(enc)
-        return i;
-    else
-        return -1;
-}
 
-/**
- * Esta função verifica se o segundo argumento contém o primeiro.
- * Por exemplo, verifica se "banana" contém "ana"
- * @return 1 se maior contém pequena, 0 caso contrário
- * @param maior A string maior
- * @param pequena A string mais pequena
- * */
-void cad_copiar ( char s1 [] , char s2 [])
-{
+        for(i = 0; !enc&&s1[i]; i++) {
+            for(j =0; !enc&&s2[j]; j++)
+                if(s1[i]==s2[j]) {
+                    enc = 1;
+                    index = i;
 
-    int i;
+                }
+        }
 
-    for ( i = 0; s1[i]; i++)
-        s2[i]=s1[i];
-    s2[i]='\0';
-
-}
-/**
- * Esta função verifica se o segundo argumento contém o primeiro.
- * Por exemplo, verifica se "banana" contém "ana"
- * @return 1 se maior contém pequena, 0 caso contrário
- * @param maior A string maior
- * @param pequena A string mais pequena
- * */
-void cad_colar ( char s1 [] , char s2 [])
-{
-
-  
-int size1 = cad_tamanho (s1);    
-int n = cad_tamanho (s2) + size1;
-    int i, j;
-
-    for(i = size1, j = 0; i < n  ; i++, j++)
-        s1[i] = s2 [j];
-    s2[i]='\0';
-}
-
-/* procura um carater numa string e devolve: -1 se não encontrou a posição do carater na string calcular tamanho de uma string*/
-void cad_mover ( char m1 [] , char m2 [] , int n )
-{
-    int i;
+        if(enc)
+            return index;
+        else
+            return -1;
+    }
+    /**
+     * Esta função verifica se o segundo argumento contém o primeiro.
+     * Por exemplo, verifica se "banana" contém "ana"
+     * @return 1 se maior contém pequena, 0 caso contrário
+     * @param maior A string maior
+     * @param pequena A string mais pequena
+     * */
+    int cad_nenhum_dos ( char s1 [] , char s2 []) {
 
 
-    for (i = 0; i < n ; i++)
-        m1 [i] = m2 [i];
+        int i , enc = 0;
+
+        for(i=0; s1[i]==s2[i]&&s1[i]&&s2[i]; i++);
+
+        if(s1[i]!=s2[i])
+            enc = 1;
 
 
-}
+        if(enc)
+            return i;
+        else
+            return -1;
+    }
+
+    /**
+     * Esta função verifica se o segundo argumento contém o primeiro.
+     * Por exemplo, verifica se "banana" contém "ana"
+     * @return 1 se maior contém pequena, 0 caso contrário
+     * @param maior A string maior
+     * @param pequena A string mais pequena
+     * */
+    void cad_copiar ( char s1 [] , char s2 []) {
+
+        int i;
+
+        for ( i = 0; s1[i]; i++)
+            s2[i]=s1[i];
+        s2[i]='\0';
+
+    }
+    /**
+     * Esta função verifica se o segundo argumento contém o primeiro.
+     * Por exemplo, verifica se "banana" contém "ana"
+     * @return 1 se maior contém pequena, 0 caso contrário
+     * @param maior A string maior
+     * @param pequena A string mais pequena
+     * */
+    void cad_colar ( char s1 [] , char s2 []) {
+
+
+        int size1 = cad_tamanho (s1);
+        int n = cad_tamanho (s2) + size1;
+        int i, j;
+
+        for(i = size1, j = 0; i < n  ; i++, j++)
+            s1[i] = s2 [j];
+        s2[i]='\0';
+    }
+
+    /* procura um carater numa string e devolve: -1 se não encontrou a posição do carater na string calcular tamanho de uma string*/
+    void cad_mover ( char m1 [] , char m2 [] , int n ) {
+        int i;
+
+
+        for (i = 0; i < n ; i++)
+            m1 [i] = m2 [i];
+
+
+    }
